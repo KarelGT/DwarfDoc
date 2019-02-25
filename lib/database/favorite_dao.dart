bf31541eb98dd17824752626e51446a4aff7d10c
KarelGT <karel.gt@icloud.com>
2019-02-25 10:26:37 +0800

    update favorite dao

--- lib/database/favorite_dao.dart
+++ lib/database/favorite_dao.dart
@@ -1,5 +1,6 @@
 import 'package:dwarf_doc/application.dart';
 import 'package:dwarf_doc/http/topic_resp.dart';
+import 'package:sqflite/sqflite.dart';
 
 class FavoriteDao {
   static const String _TABLE_NAME = "Favorite";
@@ -10,18 +11,46 @@ class FavoriteDao {
   static const String CREATE_SQL =
       'CREATE TABLE IF NOT EXISTS $_TABLE_NAME ($_ID INTEGER PRIMARY KEY, $_TOPIC_ID INTEGER, $_TOPIC_RESP TEXT)';
 
-  Future<int> saveOrUpdateTopicResp(TopicResp) async {
+  Future<int> saveOrUpdateTopicResp(TopicResp topicResp) async {
     var database = await Application.getInstance().getDatabase();
-    //Todo Karel save or update
+    var ret = await _queryTopicResps(database, topicId: topicResp.id);
+    if(ret == null || ret.isEmpty) {
+      //todo Karel save
+    } else {
+      //todo Karel update
+    }
   }
 
-  Future<List<TopicResp>> queryTopicResps({int id, int topicId}) async {
-    List<TopicResp> ret = List<TopicResp>();
+  Future<List<TopicResp>> queryTopicRespsById(int id) async {
     var database = await Application.getInstance().getDatabase();
+    var ret = await _queryTopicResps(database, id: id);
+    await database.close();
+    return ret;
+  }
+
+  Future<List<TopicResp>> queryTopicRespsByTopicId(int topicId) async {
+    var database = await Application.getInstance().getDatabase();
+    var ret = await _queryTopicResps(database, topicId: topicId);
+    await database.close();
+    return ret;
+  }
+
+  Future<List<TopicResp>> _queryTopicResps(Database database, {int id, int topicId}) async {
+    List<TopicResp> ret = List<TopicResp>();
     String where;
     List<dynamic> whereArgs;
-    database.query(_TABLE_NAME, columns: [_ID, _TOPIC_ID, _TOPIC_ID], where: where, whereArgs: whereArgs);
-    //Todo Karel query
+    if (id != null) {
+      where = '$_ID = ?';
+      whereArgs = [id];
+    } else if (topicId != null) {
+      where = '$_TOPIC_ID = ?';
+      whereArgs = [topicId];
+    }
+    var list = await database.query(_TABLE_NAME,
+        columns: [_ID, _TOPIC_ID, _TOPIC_ID],
+        where: where,
+        whereArgs: whereArgs);
+    ret = list.map((value) => TopicResp.fromJson(value)).toList();
     return ret;
   }
 }
